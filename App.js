bf452147d1393425bc2a489453ea997090e84aef
slfiene <sean.fiene@nintex.com>
2019-04-28 11:04:54 -0700

    componentsPart1

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Main from './components/MainComponent';
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Main />
+    );
+  }
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#0ff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
