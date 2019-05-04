121c4bef135dc1be4c5c2ec8a3bec87e083290a3
slfiene <sean.fiene@nintex.com>
2019-05-04 11:21:31 -0700

    Week1Assignment

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -3,6 +3,8 @@ import DishDetail from './DishdetailComponent';
 import { View, Platform } from 'react-native';
 import Menu from './MenuComponent';
 import Home from './HomeComponent';
+import About from './AboutComponent';
+import Contact from './ContactComponent';
 import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 
 const MenuNavigator = createStackNavigator({
@@ -35,6 +37,34 @@ const HomeNavigator = createStackNavigator({
   }
 });
 
+const AboutNavigator = createStackNavigator({
+  About: { screen: About }
+}, {
+  navigationOptions: {
+    headerStyle: {
+      backgroundColor: "#512DA8"
+    },
+    headerTintColor: "#fff",
+    headerTitleStyle: {
+      color: "#fff"
+    }
+  }
+});
+
+const ContactNavigator = createStackNavigator({
+  Contact: { screen: Contact }
+}, {
+  navigationOptions: {
+    headerStyle: {
+      backgroundColor: "#512DA8"
+    },
+    headerTintColor: "#fff",
+    headerTitleStyle: {
+      color: "#fff"
+    }
+  }
+});
+
 const MainNavigator = createDrawerNavigator({
   Home: {
     screen: HomeNavigator,
@@ -43,11 +73,26 @@ const MainNavigator = createDrawerNavigator({
       drawerLabel: 'Home'
     }
   },
+
+  About: {
+    screen: AboutNavigator,
+    navigationOptions: {
+      title: 'About Us',
+      drawerLabel: 'About Us'
+    }
+  },
   Menu: {
     screen: MenuNavigator,
     navigationOptions: {
       title: 'Menu',
       drawerLabel: 'Menu'
+    }
+  },
+  Contact: {
+    screen: ContactNavigator,
+    navigationOptions: {
+      title: 'Contact Us',
+      drawerLabel: 'Contact Us'
     }
   }
 }, {    drawerBackgroundColor: '#D1C4E9'});
