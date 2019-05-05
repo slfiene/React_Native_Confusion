299185de46063edd7d61b2f5f37b3f07c32ba74a
slfiene <sean.fiene@nintex.com>
2019-05-04 19:20:47 -0700

    Buttons and Icons

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -17,6 +17,16 @@ function RenderDish(props) {
                 <Text style={{margin: 10}}>
                     {dish.description}
                 </Text>
+                <Icon
+                    raised
+                    reverse
+                    name={
+                        props.favorite ? 'heart' : 'heart-o'
+                    }
+                    type="font-awesome"
+                    color="#f50"
+                    onPress={() => props.favorite ? console.log('Already favorite') : props.onPress()}
+                    />
             </Card>
         );
     }
@@ -49,17 +59,20 @@ const RenderComments =(props) => {
     );
 }
 
-// @38:13 in Buttons and Icons Video
-
 class DishDetail extends Component {
 
     constructor(props) {
         super(props);
         this.state ={
             dishes: DISHES,
-            comments: COMMENTS
+            comments: COMMENTS,
+            favorites: []
         };
     } 
+
+    markFavorite(dishId) {
+        this.setState({favorites: this.state.favorites.concat(dishId)})
+    }
 
     static navigationOptions = {
         title: 'Dish Details'
@@ -69,7 +82,9 @@ class DishDetail extends Component {
         const dishId = this.props.navigation.getParam('dishId', '');
         return(
             <ScrollView>
-                <RenderDish dish={this.state.dishes[+dishId]} />
+                <RenderDish dish={this.state.dishes[+dishId]}
+                    favorite={this.state.favorites.some(el => el === dishId)}
+                    onPress={() => this.markFavorite(dishId)} />
                 <RenderComments comments={this.state.comments.filter((comment) => comment.dishId === dishId)} />
             </ScrollView>
         
