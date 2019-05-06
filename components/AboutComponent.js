969b9c801bdfe6be8c86f713df4ed9ea6b875523
slfiene <sean.fiene@nintex.com>
2019-05-06 13:24:34 -0700

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -4,6 +4,7 @@ import { Card, ListItem } from "react-native-elements";
 import { LEADERS } from "../shared/leaders";
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
   return {
@@ -47,20 +48,42 @@ class About extends Component {
         />
       );
     };
-
-    return (
-      <ScrollView>
-        <History />
-        <Card title="Corporate Leadership">
-          <FlatList
-            data={this.props.leaders.leaders}
-            renderItem={renderLeaderItem}
-            keyExtractor={item => item.id.toString()}
-          />
-        </Card>
-      </ScrollView>
-    );
+    
+    if (this.props.leaders.isLoading) {
+      return(
+        <ScrollView>
+          <History />
+          <Card
+            title="Corporate Leadership">
+            <Loading />  
+          </Card>
+        </ScrollView>
+      );
+    } else if (this.props.leaders.errMess) {
+      return(
+        <ScrollView>
+          <History />
+          <Card
+            title="Corporate Leadership">
+            <Text>{this.props.leaders.errMess}</Text>
+          </Card>
+        </ScrollView>
+      );
+    } else {
+      return (
+        <ScrollView>
+          <History />
+          <Card title="Corporate Leadership">
+            <FlatList
+              data={this.props.leaders.leaders}
+              renderItem={renderLeaderItem}
+              keyExtractor={item => item.id.toString()}
+            />
+          </Card>
+        </ScrollView>
+      );
+    }
   }
-}
+};
 
 export default connect(mapStateToProps)(About);
