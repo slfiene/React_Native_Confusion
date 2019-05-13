83f2908354697d5b134518e248fc244056ec3676
slfiene <sean.fiene@nintex.com>
2019-05-13 15:48:29 -0700

    ChangeBaseURL for Work

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -165,7 +165,7 @@ class DishDetail extends Component {
             <Rating
               type="star"
               ratingCount={5}
-              showRating="true"
+              showRating = {true}
               minValue={1}
               onFinishRating={this.handleRating}
             />
