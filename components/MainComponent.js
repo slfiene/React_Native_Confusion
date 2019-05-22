2b1c47bc801f9a48593bd648177a4c6d777bb7df
slfiene <sean.fiene@nintex.com>
2019-05-22 00:07:18 -0700

    SecureStore

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -16,6 +16,7 @@ import { connect } from 'react-redux';
 import { fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
 import Reservation from './ReservationComponent';
 import Favorites from './FavoriteComponent';
+import Login from './LoginComponent';
 
 const mapStateToProps = state => {
     return {
@@ -178,6 +179,24 @@ const FavoritesNavigator = createStackNavigator({
   })
 });
 
+const LoginNavigator = createStackNavigator({
+  Login: { screen: Login}
+}, {
+  navigationOptions: ({navigation}) => ({
+    headerStyle: {
+      backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+      color: "#fff"
+    },
+    headerTintColor: "#fff",
+    headerLeft: <Icon name="menu" size={24}
+      color="white"
+      onPress={ () => navigation.toggleDrawer()} />
+  })
+})
+
+
 
 const CustomDrawerContentComponent = props => (
   <ScrollView>
@@ -203,6 +222,21 @@ const CustomDrawerContentComponent = props => (
 
 const MainNavigator = createDrawerNavigator(
   {
+    Login: 
+    { screen: LoginNavigator,
+      navigationOptions: {
+        title: 'Login',
+        drawerLabel: 'Login',
+        drawerIcon: ({ tintColor, focused }) => (
+          <Icon
+            name='sign-in'
+            type='font-awesome'            
+            size={24}
+            color={tintColor}
+          />
+        ),
+      }
+    },
     Home: {
       screen: HomeNavigator,
       navigationOptions: {
@@ -284,8 +318,9 @@ const MainNavigator = createDrawerNavigator(
       }
     }
   },
-  { drawerBackgroundColor: "#D1C4E9",
-contentComponent: CustomDrawerContentComponent }
+  { initialRouteName: 'Home',
+   drawerBackgroundColor: "#D1C4E9",
+   contentComponent: CustomDrawerContentComponent }
 );
 
 class Main extends Component {
